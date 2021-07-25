7c41fb9c1ccedad58644f1f9af6fea6bf7f3e1c9
dzaki236 <dzakiahnafz@gmail.com>
2021-07-25 21:19:12 +0700

    add example for api

---
+++ API/MYSQL/example/connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';//rubah ini
+$username = 'username';//rubah ini
+$password = 'passwords';//rubah ini 
+$db_name = 'contoh';//rubah ini
+$db_table = 'buku';//rubah ini
+$connection = mysqli_connect($host,$username,$password,$db_name);
+
+if(!$connection)
+{
+    echo('mysql_eror');
+}
+?>
